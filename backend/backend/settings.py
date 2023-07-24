de9aff05b1814973d6c0781caa0e46bb222ae3e8
Vasiliy <visekDI@yandex.ru>
2023-07-24 06:11:24 +0000

    pro

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,3 +1,6 @@
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -5,10 +8,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['84.201.142.105', '127.0.0.1', 'localhost', 'mazdadog.sytes.net']
 
+sentry_sdk.init(
+    dsn="https://6e1d1cf459ca47b5924744aa2fd053b8@o4505566311153664.ingest.sentry.io/4505566319935488",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+) 
 
 # Application definition
 
@@ -103,7 +114,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static_backend'
+
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
 from rest_framework import routers
-
 from api import views
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
